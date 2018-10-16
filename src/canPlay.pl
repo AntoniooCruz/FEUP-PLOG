bfc4856ff12584583eaa22b3010f32f0a57c7275
Afonso <afonsojoseazevedo@gmail.com>
2018-10-16 16:26:44 +0100

    Update src/canPlay.pl

--- src/canPlay.pl
+++ src/canPlay.pl
@@ -3,6 +3,10 @@
 createBoard(X):-
 append([[1,0,0],[0,1,0]], [[0,0,1]], X).
 
+validCondition(J, X):-
+J == 1 -> X == 0 ; X == 2.
+J == 2 -> X == 0 ; X == 1.
+
 
 vazio(0,0, [[V | W] | T]):- V == 0.
 vazio(0,0, [V | W]):- V == 0.
@@ -11,6 +15,9 @@ vazio(0, Y, [V | W]):- vazio2(0, Y, V).
 vazio(X, Y, [V | W]):- X1 is X - 1, vazio(X1, Y, W).
 vazio2(0,0, [V | W]):- vazio(0,0, [V | W]).
 vazio2(0, Y, [V | W]):- Y1 is Y - 1, vazio2(0, Y1, W).
+
+
+
 
 selectRow(0, [V | W]) :- 0 == 0.
 selectRow(Y, [V | W]) :- Y1 is Y - 1, selectRow(Y1, W). 
