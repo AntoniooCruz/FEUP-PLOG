5dbd3b3cf9292a7d5e893080dc6b57e27ef7cf1d
Unknown <acruz9823@gmail.com>
2018-10-21 11:06:41 +0100

    Changed Variables names to make code clearer

--- src/display_game.pl
+++ src/display_game.pl
@@ -1,16 +1,56 @@
 display_game(N):-
-createTable(N,[]),
+    createBoard(N,[],FinalBoard),
+    print_tab(FinalBoard,0).
+
+createBoard(N,Board,FinalBoard):-
+    (
+        N mod 2 =\= 0 -> write('The board must have an even length') ; 
+        createBoardList(N,0,Board,FinalBoard),
+        createBoard(0,0,FinalBoard,FinalBoard)
+    ).
+createBoard(0,0,FinalBoard,FinalBoard).
+
+createBoardList(N,Counter,Board,FinalBoard):-
+    N > 0,
+    Size is N + Counter,
+    (
+        N mod 2 =:= 0 -> createWhiteLine(Size,[],F1),
+        append([F1],Board,B1);
+        createBlackLine(Size,[],B2),
+        append(Board,[B2],B1)
+    ),
+    N1 is N - 1,
+    C1 is Counter + 1,
+    createBoardList(N1,C1,B1,FinalBoard).
+    createBoardList(0,Counter,Board,Board).
+    
+
+createWhiteLine(Size,Line,F):-
+    Size > 0,
+    append([1],Line,L1),
+    S1 is Size-1,
+    createWhiteLine(S1,L1,F).
+
+createWhiteLine(0,Line,Line).
+
+createBlackLine(Size,Line,F):-
+    Size > 0,
+    append([2],Line,L1),
+    S1 is Size-1,
+    createBlackLine(S1,L1,F).
+
+createBlackLine(0,Line,Line).
 
 print_tab([]).
 print_tab([L|T],X):-
+Counter is X+1,
 print_line(L),
-C is X+1,
-write(C),nl,
-print_tab(T,C).
+write(Counter),nl,
+print_tab(T,Counter).
 
 print_line([]).
-print_line([C|L]):-
-print_cell(C),
+print_line([Counter|L]):-
+print_cell(Counter),
 print_line(L).
 
 print_cell(X):- 
@@ -19,54 +59,15 @@ write('|'),
 put_code(V),
 write('|').
 
-createTable(N,L):-
-    (
-        N mod 2 =\= 0 -> write('The board must have an even length') ; 
-        createTableList(N,0,L,F),
-        print_tab(F,0)
-    ).
-
-createTableList(N,C,T,F):-
-    N > 0,
-    D is N + C,
-    (
-        N mod 2 =:= 0 -> createWhiteLine(D,[],F1),
-        append([F1],T,T1);
-        createBlackLine(D,[],F2),
-        append(T,[F2],T1)
-    ),
-    N1 is N - 1,
-    C1 is C + 1,
-    createTableList(N1,C1,T1,F).
-    createTableList(0,C,T,T).
-    
-
-createWhiteLine(D,L,F):-
-    D > 0,
-    append([1],L,L1),
-    D1 is D-1,
-    createWhiteLine(D1,L1,F).
-
-createWhiteLine(0,L,L).
-
-createBlackLine(D,L,F):-
-    D > 0,
-    append([2],L,L1),
-    D1 is D-1,
-    createBlackLine(D1,L1,F).
-
-createBlackLine(0,L,L).
-
-    
-
 
 traduz(0,0x25a1).
 traduz(1,0x26aa).
 traduz(2,0x26ab).
 
-firstLine(N):-
+firstLine(N,Counter):-
     N > 0,
-    put_code(0x2015),
-    write(''),
+    C1 is Counter + 1,
+    write(C1),
+    write(' |'),
     N1 is N - 1,
-    firstLine(N1).
+    firstLine(N1,C1).
