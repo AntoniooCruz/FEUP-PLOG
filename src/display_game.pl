fe85eadc8dc6a7658a243f2f7d4df7570a9e9345
Afonso <afonsojoseazevedo@gmail.com>
2018-11-09 10:02:29 +0000

    Started implementing function to remove pieces after a play.

--- src/display_game.pl
+++ src/display_game.pl
@@ -79,7 +79,7 @@ firstLine(N,Counter):-
     N1 is N - 1,
     firstLine(N1,C1).
 
-/*Not working for inserts in the end of a list*/
+/*Inserts an element(1) in a list(0) on index(2), returns a list(4)*/
 insert([], Element , 0, [Element | NewList]):- insert([], Element, -1, NewList).
 insert([],_,_,[]). 
 insert([H|T], Element, 0, [Element | NewList]):- insert([H|T], Element, -3, NewList).
@@ -90,8 +90,45 @@ insert(T, Element, Index1, NewList).
 createBoard(X):-
 append([[1,0,0],[0,1,0]], [[0,0,1]], X).
 
+/*Makes a players play, replaces the cell in coordinates (0,1) with the character(2), on a board(3), returns the resulting board(4)*/
 makePlay(X, Y, Player, OldB, NewB):-
 nth0(Y, OldB, Row, RestRows),
 nth0(X, Row, Element, RestOfRow),
 insert(RestOfRow, Player, X, NewRow),
 insert(RestRows,NewRow, Y, NewB).
+
+checkUp(X,Y,OldB, NewB):-
+Y1 is Y - 1, 
+nth0(Y1, OldB, Row),
+nth0(X, Row, Element),
+Element == 1 -> makePlay(X,Y1,Element,OldB,NewB), removePieces(X,Y1,Element,OldB,NewB);
+Element == 2 -> makePlay(X,Y1,Element,OldB,NewB), removePieces(X,Y1,Element,OldB,NewB).
+
+checkDown(X,Y,OldB, NewB):-
+Y1 is Y + 1, 
+nth0(Y1, OldB, Row),
+nth0(X, Row, Element),
+Element == 1 -> makePlay(X,Y1,Element,OldB,NewB), removePieces(X,Y1,Element,OldB,NewB);
+Element == 2 -> makePlay(X,Y1,Element,OldB,NewB), removePieces(X,Y1,Element,OldB,NewB).
+
+checkLeft(X,Y,OldB,NewB):-
+X1 is X - 1, 
+nth0(Y, OldB, Row),
+nth0(X1, Row, Element),
+Element == 1 -> makePlay(X1,Y,Element,OldB,NewB), removePieces(X1,Y,Element,OldB,NewB);
+Element == 2 -> makePlay(X1,Y,Element,OldB,NewB), removePieces(X1,Y,Element,OldB,NewB).
+
+checkRight(X,Y,OldB,NewB):-
+X1 is X + 1, 
+nth0(Y, OldB, Row),
+nth0(X1, Row, Element),
+Element == 1 -> makePlay(X1,Y,Element,OldB,NewB), removePieces(X1,Y,Element,OldB,NewB);
+Element == 2 -> makePlay(X1,Y,Element,OldB,NewB), removePieces(X1,Y,Element,OldB,NewB).
+
+
+removePieces(X, Y, OldB, NewB):-
+makePlay(X, Y, 5, OldB, OldB1),
+checkUp(X,Y,OldB1,NewB),
+checkDown(X,Y,OldB1,NewB),
+checkLeft(X,Y,OldB1,NewB),
+checkRight(X,Y,OldB1,NewB),
