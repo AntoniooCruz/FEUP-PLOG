6b0a0e7f7525c9080c9d063b74632d55579c8661
Afonso <afonsojoseazevedo@gmail.com>
2018-12-20 19:23:35 +0000

    Sandwich working, Sums not working

--- TP2/restrictions2.pl
+++ TP2/restrictions2.pl
@@ -12,14 +12,14 @@ BeginPos is Dim +1,
 setColumnSandwich(Board, BeginPos, StopF, Dim).
 %------------------------------------------------
 
-%Sets the Sandwich restriction (vertical orientation) for all board cells
+%Sets the Sandwich restriction (vertical orientation) for all board cells(Tests- Success(?))
 %Position start = 1, StopF = position of the last cell where the restriction will be applied
 setColumnSandwich(Board, StopF, StopF, Dim):-
-AntePos is Pos - Dim,
-PostPos is Pos + Dim,
+AntePos is StopF - Dim,
+PostPos is StopF + Dim,
 element(AntePos, Board, Before),
 element(PostPos, Board, After),
-element(Pos, Board, Elem),
+element(StopF, Board, Elem),
 Elem #= Before #\/ Elem #= After.
 
 setColumnSandwich(Board, Pos, StopF, Dim):-
@@ -34,19 +34,19 @@ setColumnSandwich(Board, NextPos, StopF, Dim).
 %---------------------------------------------------------------------------------------------
 
 
-%Sets the "Sandwich"(horizontal orientation) restriction for all board cells(!)
+%Sets the "Sandwich"(horizontal orientation) restriction for all board cells(!)(Tests- Success)
 %/ Position start = 1 / K = Rest of the division between Cell position and board dimension
 setLineSandwich([E1,E2,E3|[]], _, _):-
 E2 #= E1 #\/ E2 #= E3.
 
 
 setLineSandwich([E1,E2,E3|T], 1, Dim):-
-K1 is K + 1,
+K1 is 2,
 Kf is K1 mod Dim,
 setLineSandwich([E2,E3|T], Kf, Dim).
 
 setLineSandwich([E1,E2,E3|T], 0, Dim):-
-K1 is K + 1,
+K1 is 1,
 Kf is K1 mod Dim,
 setLineSandwich([E2,E3|T], Kf, Dim).
 
@@ -59,7 +59,7 @@ setLineSandwich([E2,E3|T], Kf, Dim).
 
 %Sets the "Sandwich"(horizontal orientation) restriction for all elements(!)
 setLinesSandwich(Board, Dim):-
-setLineSandwich(Board, 1, Dim).
+setLineSandwich(Board, 2, Dim).
 %------------------------------------------------------------------------
 
 %Sets the restriction that each line must have a length of Dim(not used)
@@ -69,7 +69,9 @@ length(H,Dim),
 setBoardLinesLength(T,Dim).
 %-------------------------------------------------------------
 
-%Sets the Sum of pieces per line to be equal(WIP!!!)
+%Sets the Sum of white pieces per line to be equal to be ExpectedSum(3*Dim/2) (!)
+
+
 setSumPiecesLines([1|T], ActualSum, ExpectedSum, Dim, Dim):-
 NewSum #= ActualSum + 1,
 NewSum #= ExpectedSum,
@@ -81,13 +83,67 @@ NewSum #= ActualSum + 1,
 NewPos is Pos +1,
 setSumPiecesLines(T, NewSum, ExpectedSum, NewPos, Dim).
 
+setSumPiecesLines([H|T], ActualSum, ExpectedSum, Dim, Dim):-
+ActualSum #= ExpectedSum,
+setSumPiecesLines(T, 0, ExpectedSum, 0, Dim).
+
 setSumPiecesLines([H|T], ActualSum, ExpectedSum, Pos, Dim):-
 NewPos is Pos +1,
 setSumPiecesLines(T, ActualSum, ExpectedSum, NewPos, Dim).
-%--------------------------------------------
+%-------------------------------------------------------------------------
 
-%"Returns" in Sum the value of the sum of pieces in a line(?)
-getSumPiecesLine([_],_,0, 0).
+%Set the sum of white pieces of each column to be Expected Sum
+setSumPiecesColumn(Board, 1, _, ColumnNum, ColumnNum, _, 1).
+setSumPiecesColumn(Board, 0, _, ColumnNum, ColumnNum, _, _).
+
+
+setSumPiecesColumn(Board, ActualSum, ExpectedSum, ColumnNum, ColumnPos, LinePos, 1):-
+NextPos is LinePos + (ColumnPos * ColumnNum),
+NextColumnPos is ColumnPos + 1,
+nth0(Board, NextPos, NextPiece),
+setSumPiecesColumn(Board, NextSum, ExpectedSum, ColumnNum, NextColumnPos, LinePos, NextPiece),
+ActualSum #= NextSum + 1.
+
+
+setSumPiecesColumn(Board, ActualSum, ExpectedSum, ColumnNum, ColumnPos, LinePos, Piece):-
+NextPos is LinePos + (ColumnPos * ColumnNum),
+NextColumnPos is ColumnPos + 1,
+nth0(Board, NextPos, NextPiece),
+setSumPiecesColumn(Board, NextSum, ExpectedSum, ColumnNum, NextColumnPos, LinePos, NextPiece),
+ActualSum #= NextSum.
+
+
+setSumPiecesColumn(Board, ActualSum, ExpectedSum, ColumnNum, 0, LinePos, 1):-
+NextPos is LinePos + (ColumnPos * ColumnNum),
+NextColumnPos is ColumnPos + 1,
+NextLinePos is LinePos + 1,
+nth0(Board, NextPos, NextPiece),
+setSumPiecesColumn(Board, NextSum, ExpectedSum, ColumnNum, NextColumnPos, LinePos, NextPiece),
+ActualSum #= NextSum + 1,
+ActualSum #= ExpectedSum,
+setSumPiecesColumn(Board, NextSum, ExpectedSum, ColumnNum, 0, NextLinePos, NextPiece).
+
+
+setSumPiecesColumn(Board, ActualSum, ExpectedSum, ColumnNum, 0, LinePos, Piece):-
+NextPos is LinePos + (ColumnPos * ColumnNum),
+NextColumnPos is ColumnPos + 1,
+NextLinePos is LinePos + 1,
+nth0(Board, NextPos, NextPiece),
+setSumPiecesColumn(Board, NextSum, ExpectedSum, ColumnNum, NextColumnPos, LinePos, NextPiece),
+ActualSum #= NextSum,
+ActualSum #= ExpectedSum,
+setSumPiecesColumn(Board, NextSum, ExpectedSum, ColumnNum, 0, NextLinePos, NextPiece).
+%----------------------------------------------------------------
+
+%Sets the sum of white pieces for all columns to be Dim/2
+setSumPiecesColumns(Board, Dim):-
+ExpectedSum is Dim/2,
+nth0(0, Board, Piece),
+setSumPiecesColumn(Board, ActualSum, ExpectedSum, Dim, 0, 0, Piece).
+%------------------------------------------------
+
+%"Returns" in Sum the value of the sum of pieces in a line(Not used)
+/*getSumPiecesLine([_],_,0, 0).
 getSumPiecesLine([Piece|T], Piece, Sum, K):-
 getSumPiecesLine(T, Piece, Sum1),
 Sum #= 1 + Sum1.
@@ -97,13 +153,13 @@ getSumPiecesLine(T, Piece, Sum1),
 Sum #= Sum1.
 %---------------------------------------------------------
 
-%Sets the value of the sum of pieces (of each color) to Value in a line(?)
+%Sets the value of the sum of pieces (of each color) to Value in a line(Not used)
 setSumPiecesLine([_],_,0).
 setSumPiecesLine([H|T], Value,Dim):-
 getSumPiecesLine(H, 1, Sum),
 getSumPiecesLine(H, 2, Sum1),
 Sum #= Value, Sum1 #= Value,
-setSumPiecesLine(T, Value).
+setSumPiecesLine(T, Value).*/
 %-----------------------------------------------------------------------
 
 %Sets the value of the sum of pieces in each line to Dim/2
@@ -157,7 +213,11 @@ length(InitialList,NumCells),
 length(SolvedBoard, NumCells),
 %domain(SolvedBoard,1,2), %Used in Sicstus
 SolvedBoard ins 1..2, % Used in SWI-Prolog
-setPiecesValues(SolvedBoard, Dim),
+transferPieceCoord(InitialList,SolvedBoard),
+setSumPiecesLines(SolvedBoard, Sum, 2, 0, 4),
+%setColumnsSandwich(SolvedBoard, BoardDim),
+%setSumPiecesLines(SolvedBoard, 0, 3, 0, BoardDim),
+%setPiecesValues(SolvedBoard, BoardDim),
 labeling([],SolvedBoard).
 %--------------------------------------------------------------------------------
 
@@ -165,6 +225,7 @@ test(I,F):-
 length(I,Dim),
 length(F,Dim),
 transferPieceCoord(I,F),
+
 labeling([],F).
 %_MISC_
 %SolvedBoard = [[A1,A2,A3,A4],[B1,B2,B3,B4],[C1,C2,C3,C4],[D1,D2,D3,D4]],
